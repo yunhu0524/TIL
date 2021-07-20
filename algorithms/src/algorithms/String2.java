d01cf7e5aedcf7e62a667967203772971f5f4d2a
yun hu choi <qnrud1202@naver.com>
2021-07-21 00:33:48 +0900

    0721-String

---
+++ algorithms/src/algorithms/String2.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+import java.util.Scanner;
+
+//대문자를 소문자로 소문자를 대문자로
+
+ class String2 {
+	public int solution(String str, char t) {
+		int answer=0;
+		str=str.toUpperCase();
+		for(char x : str.toCharArray()) {
+			if(x==t) answer++;	
+			
+		}
+	
+		return answer;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		String2 T = new String2();
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		char c = sc.next().charAt(0);
+		
+		System.out.println(T.solution(str,c));
+	}
+
+}
