97134e937a629030014dfaa6919f2b8bab448a74
yun hu choi <qnrud1202@naver.com>
2021-07-22 13:15:28 +0900

    0722

---
+++ algorithms/src/algorithms/String5.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+//알파벳일 경우에만 뒤집기
+import java.util.Scanner;
+
+class String5 {
+	public String solution(String str){
+		String answer = "";
+		char [] s = str.toCharArray(); // str 단어를 문자 배열로  s 에 저장
+		int lt=0, rt=str.length()-1;
+		while(lt<rt) {
+			if(!Character.isAlphabetic(lt)) {
+				lt++;
+			}
+			else if (!Character.isAlphabetic(rt)) {
+				rt--;
+			}
+			else {
+				char tmp=s[lt];
+				s[lt]=s[rt];
+				s[rt]=tmp;
+				lt++;
+				rt--;
+			}
+		}
+			answer = String.valueOf(s);
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		String5 T = new String5();
+		Scanner sc = new Scanner(System.in);
+		
+		String  str = new String();
+		System.out.println(T.solution(str));
+	}
+
+}
