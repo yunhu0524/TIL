44a3c926862385b18ce1cb97fb0778d96634ec94
yunhu <cyh5237@tidy-b.com>
2021-09-29 23:10:48 +0900

    TIL

--- html_css_javascript/javascript/니코_javascript/app.js
+++ html_css_javascript/javascript/니코_javascript/app.js
@@ -2,3 +2,12 @@ function plus(a,b){
   console.log(a+b);
 }
 plus(8,60);
+
+const player = {
+  name:"nico",
+  sayHello: function(){
+    console.log("hello");
+  }
+};
+console.log(player.name);
+player.sayHello();
