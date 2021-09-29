6309072e6b2f1a58d552fa2ad85a6a8d51c6b6a2
yunhu <cyh5237@tidy-b.com>
2021-09-29 23:29:41 +0900

    .

--- html_css_javascript/javascript/니코_javascript/app.js
+++ html_css_javascript/javascript/니코_javascript/app.js
@@ -11,3 +11,11 @@ const player = {
 };
 console.log(player.name);
 player.sayHello("haha");
+
+const toBuy=["potato","tomato","pizza"];
+
+console.log(toBuy);
+toBuy[2] = "water";
+console.log(toBuy);
+toBuy.push("meat");
+console.log(toBuy);
