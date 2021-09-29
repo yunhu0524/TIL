ef0089b64bedfab41f1961393f3e23fd8999820c
yunhu <cyh5237@tidy-b.com>
2021-09-29 23:50:56 +0900

    til

--- html_css_javascript/javascript/니코_javascript/app.js
+++ html_css_javascript/javascript/니코_javascript/app.js
@@ -3,6 +3,8 @@ function plus(a,b){
 }
 plus(8,60);
 
+
+// function
 const player = {
   name:"nico",
   sayHello: function(a){
@@ -12,6 +14,8 @@ const player = {
 console.log(player.name);
 player.sayHello("haha");
 
+
+// 배열
 const toBuy=["potato","tomato","pizza"];
 
 console.log(toBuy);
@@ -19,3 +23,11 @@ toBuy[2] = "water";
 console.log(toBuy);
 toBuy.push("meat");
 console.log(toBuy);
+
+// return
+const age = 150;
+function calculateKrAge(ageOfForeigner){
+ return ageOfForeigner + 2;
+}
+const krAge = calculateKrAge(age);
+console.log(krAge);
