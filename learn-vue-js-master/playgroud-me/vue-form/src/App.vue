b01a02e0ae819d5454acf8bf39186206e49f475f
yunhu <cyh5237@tidy-b.com>
2021-10-09 21:09:24 +0900

    vur-form-data

---
+++ learn-vue-js-master/playgroud-me/vue-form/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return{
+      username:'',
+      password:'',
+    }
+  },
+  methods:{
+    submitForm(){
+      let url = 'https://jsonplaceholder.typicode.com/users'
+      let data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(function(res){
+          console.log(res);
+        })
+        .catch(function(err){
+          console.log(err);
+        })
+      console.log(this.username, this.password);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
