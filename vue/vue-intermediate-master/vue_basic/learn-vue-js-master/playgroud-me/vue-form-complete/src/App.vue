a845f2879beed16abc19e8d9a407cbc3298ab30e
yunhu <cyh5237@tidy-b.com>
2021-10-10 23:52:58 +0900

    TIL

---
+++ vue/vue-intermediate-master/vue_basic/learn-vue-js-master/playgroud-me/vue-form-complete/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{ str }}
+     <app-header 
+     v-bind:propsdata="str"
+     v-on:renew="renewStr"></app-header>
+  </div>
+ 
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  data(){
+    return {
+      str :'Header'
+    }
+  },
+  components:{
+    'app-header':AppHeader
+  },
+  methods:{
+    renewStr(){
+      this.str = 'hi'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
