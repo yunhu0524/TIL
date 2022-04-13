f7d99c8c4887f4080dafb4fcf879eaf5e2111886
yunhu <cyh5237@tidy-b.com>
2022-04-14 00:02:26 +0900

    4_14

---
+++ LeetCode/3_Longest_Substring_Without_Repeating_Characters.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+let s = "abcccdef";
+
+var lengthOfLongestSubstring = function (s) {
+  let start = 0;
+  let temp = "";
+  let result = "";
+
+  // console.log(s.length);
+  for (let i = 0; i < s.length; i++) {
+    let index = temp.indexOf(s[i]);
+    console.log(`temp ::: ${temp}`);
+    console.log(`s[i]::: ${s[i]}`);
+    // console.log(index);
+    if (index !== -1) {
+      start = start + index + 1;
+      console.log(`start::: ${start}`);
+    }
+
+    temp = s.substring(start, i + 1);
+
+    if (result.length < temp.length) {
+      result = temp;
+    }
+  }
+  return result.length;
+};
+
+console.log(lengthOfLongestSubstring(s));
