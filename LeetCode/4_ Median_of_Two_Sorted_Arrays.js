9e573daaa143ae36b0656a613f1838013714b36d
yunhu <cyh5237@tidy-b.com>
2022-04-15 23:26:39 +0900

    4_15

--- LeetCode/4_ Median_of_Two_Sorted_Arrays.js
+++ LeetCode/4_ Median_of_Two_Sorted_Arrays.js
@@ -6,17 +6,21 @@
  */
 
 let nums1 = [1, 3];
-let nums2 = [3, 4];
+let nums2 = [3, 4, 5];
 const sum = (...args) => args.reduce((acc, cur) => acc + cur, 0);
-console.log(sum(nums1));
 
 var findMedianSortedArrays = function (nums1, nums2) {
-  const nums = nums1.concat(nums2);
-  nums.sort((a, b) => a - b);
+  const nums = nums1.concat(nums2); // 배열
+  nums.sort((a, b) => a - b); // 오름차순 정렬
 
-  const mid = parseInt(nums.length / 2);
+  const mid = parseInt(nums.length / 2); // 중앙 값
 
-  return nums.length % 2 === 0
+  // console.log(parseInt(nums.length));
+  // console.log(mid);
+  // console.log(sum(nums[mid - 1], nums[mid] / 2).toFixed(5));
+  // console.log(nums[mid].toFixed(5));
+
+  return nums.length % 2 === 0 // 홀수 짝수 구분
     ? (sum(nums[mid - 1], nums[mid]) / 2).toFixed(5)
     : nums[mid].toFixed(5);
 };
