e91496161591d3f16d0a5a222b4535eba4fdb6e3
yunhu <cyh5237@tidy-b.com>
2022-08-09 00:27:45 +0900

    13

--- LeetCode/13_Roman to Integer.js
+++ LeetCode/13_Roman to Integer.js
@@ -1,6 +1,34 @@
 var romanToInt = function (s) {
-  const strArr = s.split('');
-  console.log(strArr);
+  const strArr = s.split("");
+  const numArr = strArr.map((s) => {
+    switch (s) {
+      case "I":
+        return 1;
+      case "V":
+        return 5;
+      case "X":
+        return 10;
+      case "L":
+        return 50;
+      case "C":
+        return 100;
+      case "D":
+        return 500;
+      case "M":
+        return 1000;
+    }
+  });
+  let sum = numArr[numArr.length - 1];
+
+  for(let i = numArr.length-1; i>=0; i--){
+    if(i-1 < 0) break;
+    if (numArr[i] > numArr[i-1]){
+      sum -= numArr[i-1];
+    }else{
+      sum += numArr[i-1];
+    }
+  }
+  return sum;
 };
 
-console.log(romanToInt(1100));
+console.log(romanToInt('III'));
