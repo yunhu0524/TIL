b1336c930a6b787a5f161663b7ec4c9c6cd5e9bb
yunhu <cyh5237@tidy-b.com>
2022-08-10 00:23:35 +0900

    14

--- LeetCode/14_Longest Common Prefix.js
+++ LeetCode/14_Longest Common Prefix.js
@@ -3,21 +3,33 @@
 // 3. 일치하지 않으면 루프를 벗어난다.
 
 var longestCommonPrefix = function (str) {
+  // 같은 글자
+  let sameWord = "";
+  // str개수가 1일때. 
+  if (str.length === 1) {
+    return (sameWord = str[0]);
+  }
   // str 에 첫번째 단어 길이
   const firstWordLength = str[0].length;
-
   // str 에서 제일 작은 길이에 단어
   let minLength = null;
-  let sameWord = [];
+
+  // 제일 작은 길이 단어 찾기
   for (let i = 0; i < str.length - 1; i++) {
     minLength = Math.min(firstWordLength, str[i].length);
   }
+  // minLength 로 sameWord 찾기
   for (let i = 0; i < minLength; i++) {
     let current = str[0][i];
-    console.log(current);
+    for (let j = 0; j < str.length; j++) {
+      if (str[j][i] !== current) {
+        return sameWord;
+      }
+    }
+    sameWord += current;
   }
-  console.log(minLength);
+  return sameWord;
 };
 
-const strs = ["flower", "flow", "flight"];
+const strs = ["a"];
 console.log(longestCommonPrefix(strs));
