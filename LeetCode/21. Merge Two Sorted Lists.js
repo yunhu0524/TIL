373bf3eb84fec9f8aba5a0d6ca063fb72794fbd3
choiyunhu <cyh5237@tidy-b.com>
2022-08-17 23:46:40 +0900

    22

--- LeetCode/21. Merge Two Sorted Lists.js
+++ LeetCode/21. Merge Two Sorted Lists.js
@@ -1,7 +1,6 @@
 var mergeTwoLists = function (l1, l2) {
-  // if (!l1 || !l2) return l1 ? l1 : l2;
+  if (!l1 || !l2) return l1 ? l1 : l2;
   if (l1.val < l2.val) {
-    console.log(l1.val);
     l1.next = mergeTwoLists(l1.next, l2);
     return l1;
   } else {
