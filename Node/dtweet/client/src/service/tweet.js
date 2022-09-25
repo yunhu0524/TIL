f3ada9e987027c59e895b416db1b53aa011bc2df
choiyunhu <cyh5237@tidy-b.com>
2022-09-25 23:12:35 +0900

    node

--- Node/dtweet/client/src/service/tweet.js
+++ Node/dtweet/client/src/service/tweet.js
@@ -1,43 +1,69 @@
-export default class TweetService {
-  tweets = [
-    {
-      id: 1,
-      text: '드림코딩에서 강의 들으면 너무 좋으다',
-      createdAt: '2021-05-09T04:20:57.000Z',
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-    },
-  ];
+// import user from "../../../../7-routing/routes/user";
 
-  async getTweets(username) {
-    return username
-      ? this.tweets.filter((tweet) => tweet.username === username)
-      : this.tweets;
+export default class TweetService {
+  // tweets = [
+  //   {
+  //     id: 1,
+  //     text: '드림코딩에서 강의 들으면 너무 좋으다',
+  //     createdAt: '2021-05-09T04:20:57.000Z',
+  //     name: 'Bob',
+  //     username: 'bob',
+  //     url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+  //   },
+  // ];
+
+  constructor(baseURL) {
+    this.baseURL = baseURL;
   }
 
+  // tweets 가져오기
+  async getTweets(username) {
+    const query = username ? `?username=${username}` : '';
+    const res = await fetch(`${this.baseURL}/tweets${query}`, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+    });
+    const data = await res.json();
+    if (res.status !== 200) {
+      throw new Error((data.message));
+    }
+    return data;
+  }
+
+  // tweet 보내기
   async postTweet(text) {
-    const tweet = {
-      id: Date.now(),
-      createdAt: new Date(),
-      name: 'Ellie',
-      username: 'ellie',
-      text,
-    };
-    this.tweets.push(tweet);
-    return tweet;
+    const res = await fetch(`${this.baseURL}/tweets`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({text, username:'ellie', name:'Ellie'})
+    });
+    const data = await res.json();
+    if (res.status !== 201) {
+      throw new Error((data.message));
+    }
+    return data;
   }
 
   async deleteTweet(tweetId) {
-    this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
+    const res = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+      method: 'DELETE',
+      headers: {'Content-Type': 'application/json'},
+    });
+    if (res.status !== 204) {
+      throw new Error();
+    }
   }
 
   async updateTweet(tweetId, text) {
-    const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
-    if (!tweet) {
-      throw new Error('tweet not found!');
+    const res = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({text})
+    });
+    const data = await res.json();
+    if (res.status !== 200) {
+      throw new Error((data.message));
     }
-    tweet.text = text;
-    return tweet;
+    return data;
   }
 }
